3855b479e7d08c7f41b5af45744ef78b33067aa3
Luiz Rocco <luizgustavorocco@gmail.com>
2023-12-02 09:20:38 -0300

    Solves problem 1 and adds problem 2 files

--- 2023/day1_test.ts
+++ 2023/day1_test.ts
@@ -12,42 +12,70 @@ Deno.test("Problem 1", () => {
   assertEquals(problem1, 54644);
 });
 
-// const problem2 = input.split("\n").map(
-//   (
-//     line,
-//   ) => [
-//     line.match(/\d|one|two|three|four|five|six|seven|eight|nine/),
-//     line.reverse,
-//   ],
-// ).filter((matches) => matches.length > 0).map(
-//   (matches) =>
-//     [matches.at(0), matches.at(-1)].map((match) => match?.at(0)).map((el) => {
-//       switch (el) {
-//         case "one":
-//           return "1";
-//         case "two":
-//           return "2";
-//         case "three":
-//           return "3";
-//         case "four":
-//           return "4";
-//         case "five":
-//           return "5";
-//         case "six":
-//           return "6";
-//         case "seven":
-//           return "7";
-//         case "eight":
-//           return "8";
-//         case "nine":
-//           return "9";
-//         default:
-//           return el;
-//       }
-//     }),
-// ).map((arr) => arr.join("")).map(Number).reduce((acc, curr) => acc + curr, 0);
+const myMap: { [key: string]: string } = {
+  "1": "1",
+  "2": "2",
+  "3": "3",
+  "4": "4",
+  "5": "5",
+  "6": "6",
+  "7": "7",
+  "8": "8",
+  "9": "9",
+  "one": "1",
+  "two": "2",
+  "three": "3",
+  "four": "4",
+  "five": "5",
+  "six": "6",
+  "seven": "7",
+  "eight": "8",
+  "nine": "9",
+};
 
-// console.log({ problem2 });
-// Deno.test("Day Problem 2", () => {
-//   assertEquals(problem2, 53355);
-// });
+const problem2 = input.split("\n").map(
+  (
+    line,
+  ) => {
+    const firstMatchArr = line.match(
+      /\d|one|two|three|four|five|six|seven|eight|nine/g,
+    );
+
+    if (!firstMatchArr) return [];
+
+    const firstMatch = firstMatchArr[0];
+
+    const partialResult2 = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+    ].map((el) => line.lastIndexOf(el));
+
+    const indexOfPartialResult2 = Math.max(...partialResult2);
+
+    const winner = (partialResult2.indexOf(indexOfPartialResult2) + 1) % 9;
+    const secondMatch = winner === 0 ? "9" : String(winner);
+    return [myMap[firstMatch], secondMatch];
+  },
+).filter((matches) => matches.length > 0).map((arr) => arr.join("")).map(Number)
+  .reduce((acc, curr) => acc + curr, 0);
+
+console.log({ problem2 });
+Deno.test("Day Problem 2", () => {
+  assertEquals(problem2, 53348);
+});
