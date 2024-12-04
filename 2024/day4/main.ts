ceee6150d5a7bcb8a4e09a85854a3318af86ab7c
Luiz Rocco <luizgustavorocco@gmail.com>
2024-12-03 23:34:50 -0300

    2024 day 4 init

---
+++ 2024/day4/main.ts
@@ -0,0 +1,13 @@
+const main = () => {
+  // Problem 1:
+  const problem1 = () => {};
+
+  console.log("problem 1: ", problem1());
+
+  // Problem 2:
+  const problem2 = () => {};
+
+  console.log("problem 2: ", problem2());
+};
+
+main();
